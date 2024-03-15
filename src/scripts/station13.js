6af5161857dfd557015e966ee9c4bb5b01d2d19f
ittyi <vioiv.k4@gmail.com>
2024-03-16 02:45:52 +0900

    station13: チェックボックスの判定処理

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,11 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-
+  let element = document.getElementById('check');
+  if (element.checked) {
+    const button = document.querySelector("#text");
+    button.setAttribute("style", "background-color: red");
+  } else {
+    const button = document.querySelector("#text");
+    button.setAttribute("style", "background-color: transparent");
+  }
 }
